53fd4b3071f9495415004e7604808e10591396cd
chriskontsis <cak0927@gmail.com>
2022-07-01 10:16:46 -0700

    landing page css

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -7,21 +7,23 @@ const UserInput = ({ nextStep, handleChange, values }) => {
   };
 
   return (
-    <div id="page-container">
-      <div>
+    <div id="page-container"> 
+      <div id="img-container">
       <img id="landing-img" src={require("../imgs/Movie Night-amico.png")} />
       </div>
 
       <div id="form-container">
-        <form>
-          Enter A Movie
-          <input
+
+    
+        <form id="container-items">
+          <h1 id="title">Enter A Movie <br /></h1>
+          <input id="input-form"
             type="text"
             placeholder="Enter a movie"
             value={values.userMovie}
             onChange={handleChange("userMovie")}
-          />
-          <button onClick={Continue}>Next</button>
+          /> <br />
+          <button id="next-button" onClick={Continue}>Next</button>
         </form>
       </div>
     </div>
