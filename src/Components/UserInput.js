2e9bbf337c1abd5982032f3aa7ed3e0256bf2f33
chriskontsis <cak0927@gmail.com>
2022-06-30 20:10:13 -0700

    Homepage stlying

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,22 +1,30 @@
 import React from "react";
+import "../App.css";
 
 const UserInput = ({ nextStep, handleChange, values }) => {
-    const Continue = e => {
-        nextStep();
-      }
+  const Continue = (e) => {
+    nextStep();
+  };
 
   return (
-    <label>
-      Movie
-      <input
-        type="text"
-        placeholder="Enter a movie"
-        value={values.userMovie}
-        onChange={handleChange('userMovie')}
-      />
-      <button onClick={ Continue }>Next</button>
-    </label>
-    
+    <div id="page-container">
+      <div>
+      <img id="landing-img" src={require("../imgs/Movie Night-amico.png")} />
+      </div>
+
+      <div id="form-container">
+        <form>
+          Enter A Movie
+          <input
+            type="text"
+            placeholder="Enter a movie"
+            value={values.userMovie}
+            onChange={handleChange("userMovie")}
+          />
+          <button onClick={Continue}>Next</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
